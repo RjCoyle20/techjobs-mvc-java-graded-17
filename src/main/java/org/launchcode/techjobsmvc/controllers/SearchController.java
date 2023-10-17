084cfe477565fdb01f60400abe513a4da6c714fe
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2023-10-16 20:30:06 -0500

    Passing all Tests!

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -32,6 +32,7 @@ public class SearchController {
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm) {
         ArrayList<Job> jobs;
+        model.addAttribute("columns", columnChoices);
         if (searchTerm.equalsIgnoreCase("all") || (searchTerm.isEmpty())) {
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
